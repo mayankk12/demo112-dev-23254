26c7a32a8c193dfe79aa3223eb8243dcb19ea941
mayankk12 <95089081+mayankk12@users.noreply.github.com>
2021-12-20 13:47:03 +0530

    to work succesfully
    code upgraded

--- android/app/src/main/java/com/demo112_dev_23254/MainActivity.java
+++ android/app/src/main/java/com/demo112_dev_23254/MainActivity.java
@@ -4,10 +4,34 @@ import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
   @Override
   protected String getMainComponentName() {
     return "demo112_dev_23254";
